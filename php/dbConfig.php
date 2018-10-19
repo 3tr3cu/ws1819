fe0096910d66e6fef90153fe5fabc4976ee5d9cb
Endika Trecu <3nd1c4p70r@gmail.com>
2018-10-19 12:46:44 +0200

    Final fixes

--- php/dbConfig.php
+++ php/dbConfig.php
@@ -1,12 +1,12 @@
 <?php
 
-$remote=0; 
+$remote=1; 
 
 
 if ($remote) {
    $zerbitzaria="localhost";
    $erabiltzailea="id7131957_potato";
-   $gakoa="potato";  // GitHub-en eremu hau EZABATU
+   $gakoa="";  // GitHub-en eremu hau EZABATU
    $db="id7131957_quiz";        
 } else {
    $zerbitzaria="localhost";
